4abdad76e5d0e623f8bfce0921629b8f0166483f
Chintan <chintan@wohlig.com>
2016-08-18 08:35:29 +0530

    MMM

--- index.js
+++ index.js
@@ -1,138 +1,140 @@
-
-module.exports = {
-    saveData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        var foreignKeys = Config.getForeignKeys(schema);
-        if (data._id) {
-            Model.findOne({
-                _id: data._id
-            }, function(err, data2) {
-                if (err) {
-                    callback(err, data2);
-                } else if (data2) {
-                    async.each(foreignKeys, function(n, callback) {
-                        if (data[n.name] != data2[n.name]) {
-                            Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "delete", function(err, md) {
-                                if (err) {
-                                    callback(err, md);
-                                } else {
-                                    Config.manageArrayObject(mongoose.models[n.ref], data[n.name], data2._id, n.key, "create", callback);
-                                }
-                            });
-                        } else {
-                            callback(null, "no found for ");
-                        }
-                    }, function(err) {
-                        data2.update(data, {
-                            w: 1
-                        }, callback);
-                    });
-
-
-                } else {
-                    callback("No Data Found", data2);
-                }
-            });
-        } else {
-
-            Const.save(function(err, data2) {
-                if (err) {
-                    callback(err, data2);
-                } else {
-
-                    async.each(foreignKeys, function(n, callback) {
-                        Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "create", function(err, md) {
-                            callback(err, data2);
-                        });
-                    }, function(err) {
-                        callback(err, data2);
-                    });
-
-                }
-            });
-
-        }
-
-    },
-    deleteData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        var foreignKeys = Config.getForeignKeys(schema);
-        Config.checkRestrictedDelete(Model, schema, {
-            _id: data._id
-        }, function(err, value) {
-            if (err) {
-                callback(err, null);
-            } else if (value) {
+module.exports = function(schema) {
+    var data = {
+        saveData: function(data, callback) {
+            var Model = this;
+            var Const = this(data);
+            var foreignKeys = Config.getForeignKeys(schema);
+            if (data._id) {
                 Model.findOne({
                     _id: data._id
-                }).exec(function(err, data2) {
+                }, function(err, data2) {
                     if (err) {
-                        callback("Error Occured", null);
+                        callback(err, data2);
                     } else if (data2) {
                         async.each(foreignKeys, function(n, callback) {
-                            Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "delete", function(err, md) {
+                            if (data[n.name] != data2[n.name]) {
+                                Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "delete", function(err, md) {
+                                    if (err) {
+                                        callback(err, md);
+                                    } else {
+                                        Config.manageArrayObject(mongoose.models[n.ref], data[n.name], data2._id, n.key, "create", callback);
+                                    }
+                                });
+                            } else {
+                                callback(null, "no found for ");
+                            }
+                        }, function(err) {
+                            data2.update(data, {
+                                w: 1
+                            }, callback);
+                        });
+
+
+                    } else {
+                        callback("No Data Found", data2);
+                    }
+                });
+            } else {
+
+                Const.save(function(err, data2) {
+                    if (err) {
+                        callback(err, data2);
+                    } else {
+
+                        async.each(foreignKeys, function(n, callback) {
+                            Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "create", function(err, md) {
                                 callback(err, data2);
                             });
                         }, function(err) {
-                            if (err) {
-                                callback(err, md);
-                            } else {
-                                data2.remove({}, function(err, data3) {
-                                    if (err) {
-                                        callback(err, data3);
-                                    } else {
-                                        callback(err, data3);
-                                    }
-                                });
-                            }
+                            callback(err, data2);
                         });
+
                     }
                 });
-            } else if (!value) {
-                callback("Can not delete the Object as Restricted Deleted Points are available.", null);
+
             }
-        });
-    },
-    getOne: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        Model.findOne({
-            _id: data._id
-        }).deepPopulate(data.deep).exec(callback);
-    },
-    search: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        var maxRow = Config.maxRow;
 
-        var page = 1;
-        if (data.page) {
-            page = data.page;
-        }
-        var field = data.field;
-        var options = {
-            field: data.field,
-            filters: {
-                keyword: {
-                    fields: ['name'],
-                    term: data.keyword
+        },
+        deleteData: function(data, callback) {
+            var Model = this;
+            var Const = this(data);
+            var foreignKeys = Config.getForeignKeys(schema);
+            Config.checkRestrictedDelete(Model, schema, {
+                _id: data._id
+            }, function(err, value) {
+                if (err) {
+                    callback(err, null);
+                } else if (value) {
+                    Model.findOne({
+                        _id: data._id
+                    }).exec(function(err, data2) {
+                        if (err) {
+                            callback("Error Occured", null);
+                        } else if (data2) {
+                            async.each(foreignKeys, function(n, callback) {
+                                Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "delete", function(err, md) {
+                                    callback(err, data2);
+                                });
+                            }, function(err) {
+                                if (err) {
+                                    callback(err, md);
+                                } else {
+                                    data2.remove({}, function(err, data3) {
+                                        if (err) {
+                                            callback(err, data3);
+                                        } else {
+                                            callback(err, data3);
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                } else if (!value) {
+                    callback("Can not delete the Object as Restricted Deleted Points are available.", null);
                 }
-            },
-            sort: {
-                asc: 'name'
-            },
-            start: (page - 1) * maxRow,
-            count: maxRow
-        };
+            });
+        },
+        getOne: function(data, callback) {
+            var Model = this;
+            var Const = this(data);
+            Model.findOne({
+                _id: data._id
+            }).deepPopulate(data.deep).exec(callback);
+        },
+        search: function(data, callback) {
+            var Model = this;
+            var Const = this(data);
+            var maxRow = Config.maxRow;
 
-        var Search = Model.find(data.filter)
+            var page = 1;
+            if (data.page) {
+                page = data.page;
+            }
+            var field = data.field;
+            var options = {
+                field: data.field,
+                filters: {
+                    keyword: {
+                        fields: ['name'],
+                        term: data.keyword
+                    }
+                },
+                sort: {
+                    asc: 'name'
+                },
+                start: (page - 1) * maxRow,
+                count: maxRow
+            };
 
-        .order(options)
-            .deepPopulate(data.deep)
-            .keyword(options)
-            .page(options, callback);
+            var Search = Model.find(data.filter)
 
-    }
+            .order(options)
+                .deepPopulate(data.deep)
+                .keyword(options)
+                .page(options, callback);
+
+        }
+    };
+    return data;
 };
